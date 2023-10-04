7a0bb0461933cd47087248725ba0bccc668cde1f
Adam Smith <adam@adamsmith.as>
2023-10-04 15:30:29 -0700

    Update welcome message

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <h1>Vite + TypeScript</h1>
+    <h1>Hello from Lecture 3</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
