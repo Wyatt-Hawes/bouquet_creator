5cf6ae225f6eb325c890b558fd150e9d495056a3
Wyatt H <jhawes@ucsc.edu>
2023-10-15 04:37:00 -0700

    Step 4 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import "./style.css";
 
 const drawingChanged = new CustomEvent("drawing-changed");
+type ClickHandler = () => void;
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Wyatt's Sticker Sketchpad";
 const header = document.createElement("h1");
 let lines: { x: number; y: number }[][] = [];
 let currentLine: { x: number; y: number }[] | null = [];
+let redoLines: { x: number; y: number }[][] = [];
 
 document.title = gameName;
 header.innerHTML = gameName;
@@ -27,7 +29,10 @@ ctx.fillRect(0, 0, 256, 256);
 const cursor = { active: false, x: 0, y: 0 };
 addCanvasEvents();
 
-addClearButton();
+app.append(document.createElement("br"));
+addButton("undo", undoCanvas);
+addButton("redo", redoCanvas);
+addButton("clear", eraseCanvas);
 
 //-----------------------
 //-----FUNCTIONS-------//
@@ -54,6 +59,7 @@ function addCanvasEvents() {
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
     currentLine!.push({ x: cursor.x, y: cursor.y });
+    redoLines = [];
 
     canvas.dispatchEvent(drawingChanged);
   });
@@ -86,20 +92,40 @@ function redraw() {
 function clearCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "white";
-
   ctx.fillRect(0, 0, 256, 256);
 }
 
-function addClearButton() {
+function eraseCanvas() {
+  redoLines = lines;
+  lines = [];
+
+  clearCanvas();
+}
+
+function undoCanvas() {
+  if (lines.length == 0) {
+    return;
+  }
+  redoLines.push(lines.pop()!);
+  canvas.dispatchEvent(drawingChanged);
+}
+
+function redoCanvas() {
+  if (redoLines.length == 0) {
+    return;
+  }
+  lines.push(redoLines.pop()!);
+  canvas.dispatchEvent(drawingChanged);
+}
+
+function addButton(name: string, func: ClickHandler) {
   //Add clear button
-  const clearButton = document.createElement("button");
-  clearButton.innerHTML = "clear";
-  app.append(document.createElement("br"));
-  app.append(clearButton);
+  const button = document.createElement("button");
+  button.innerHTML = name;
+  app.append(button);
 
   //Add click functionality
-  clearButton.addEventListener("click", () => {
-    clearCanvas();
-    lines = [];
+  button.addEventListener("click", () => {
+    func();
   });
 }
