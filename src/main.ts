e3c77edf5b4e96b54776b19a7ddefdc7d57bc559
Wyatt H <jhawes@ucsc.edu>
2023-10-15 02:51:21 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,22 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My game";
+const gameName = "Wyatt's Sticker Sketchpad";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.id = "canvas";
+canvas.width = 256;
+canvas.height = 256;
+
+const canvasCTX = canvas.getContext("2d");
+canvasCTX!.fillStyle = "white";
+
+canvasCTX?.fillRect(0, 0, 256, 256);
+
+app.append(canvas);
